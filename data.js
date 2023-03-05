dcf33c56dae16c37dc7049d9839952b78c19b2df
Ali Shata <alishata128@gmail.com>
2023-03-06 01:09:42 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,28 @@
+const orders = [
+    {
+        id: 1,
+        icon: "🍕",
+        name: "pizza",
+        ingreds: ["pepperoni", "mushroom", "mozarella"],
+        price: 14
+    },
+    {
+        id: 2,
+        icon: "🍔",
+        name: "hamburger",
+        ingreds: ["beef", "cheese", "lettuce"],
+        price: 12
+
+    },
+    {
+
+        id: 3,
+        icon: "🧃",
+        name: "juice",
+        ingreds: ["water", "sugar", "fruit"],
+        price: 12
+
+    },
+]
+
+export default orders
